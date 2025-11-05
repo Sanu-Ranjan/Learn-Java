b026602f025e094e91b45308c7eccdfd31543aa5
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-11-05 12:48:29 +0530

    first commit

---
+++ day5/from book/constructor7.1.java
@@ -0,0 +1,35 @@
+
+class Jtc76{
+    public static void main(String args[]){
+        Hello h1= new Hello();
+        h1.m1();
+        Hello h2 = new Hello(111);
+        h2.m1();
+        Hello h3 = new Hello(111,222);
+        h3.m1();
+    }
+}
+
+
+
+class Hello{
+    int a;
+    int b;
+    static int c=30;
+    Hello(){
+        System.out.println("Default constructor in Hello");
+    }
+    Hello(int a1){
+        System.out.println("1 param constructor in hello");
+        a=a1;
+    }
+    Hello (int a1, int b1){
+        System.out.println("2 param constructor in hello");
+    }
+    void m1(){
+        System.out.println("m1 in hello");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
