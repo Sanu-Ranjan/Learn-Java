b2dfc2b93d6197bfe36388d3cbcd578ce223ff0b
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-11-06 18:47:21 +0530

    home work day 4

---
+++ day4/6.13.java
@@ -0,0 +1,24 @@
+class Hai {
+    void m1() {
+        System.out.println("m1 in Hello");
+    }
+}
+
+class Hello {
+    static Hai hai = new Hai();
+
+    static Hai getHai() {
+        System.out.println("getHai() in Hello");
+        return hai;
+    }
+}
+
+class Jtc64 {
+    public static void main(String args[]) {
+        Hai hai = Hello.getHai();
+        Hai hai1 = Hello.getHai();
+
+        System.out.println(hai.hashCode());
+        System.out.println(hai1.hashCode());
+    }
+}
